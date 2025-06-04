0b83b96b9ec0ad4b9c1119f6258da22b5a3a1e49
Espinoza08 <70042605+Espinoza08@users.noreply.github.com>
2025-06-04 14:33:21 -0600

    Se agrego el metodo 10

--- HelloController.java
+++ HelloController.java
@@ -60,6 +60,11 @@ public class HelloController {
     public String saludar9(){
         return "Hola Mundo con REST en Spring Boot";
     }
+	
+	@GetMapping("/hello10")
+    public String saludar10(){
+        return "Hola Mundo con REST en Spring Boot";
+    }
 }
 
 
