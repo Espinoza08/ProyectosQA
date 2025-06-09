493b834369e930db59b10f99a5dd0747d9f41c52
Espinoza08 <70042605+Espinoza08@users.noreply.github.com>
2025-06-09 14:50:42 -0600

    Se modifica el mensaje "Hola mundo java"

---
+++ Hello.java
@@ -0,0 +1,17 @@
+package mx.quality.services.testing.gitJenkins;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("HOLA MUNDO GITHUB - Jenkins");
+
+	}
+
+}
